fb3b707c7c84c4b9d0c427a9103ef200b348ef09
sunny <sunny.722@hotmail.com>
2020-08-29 22:23:09 -0600

    create model

--- Newcomer_machine/settings.py
+++ Newcomer_machine/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'compressor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+STATICFILES_FINDERS=[
+    'compressor.finders.CompressorFinder',
+]
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
